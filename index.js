148ce2ae1984df2ecff74ee5b459d0c2b8ee5edf
Nurbike Aytar <nurbike0@gmail.com>
2021-03-07 23:15:48 +0300

    project finished

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var readlineSync = require('readline-sync');
+console.log('"Guess the number between 0 and 20"');
 
-var ad =  readlineSync.question("Adınızı Yazın: ");
-console.log(ad);
+var randomNumber = Math.floor(Math.random() * 20);
+var turn = 0;
+
+while (true) {
+    turn++;
+    var x =  readlineSync.question("GUESS IT: ");
+    if (x == randomNumber) {
+        console.log("you won!!!" + randomNumber);
+        console.log(turn);
+        break;
+   } else if (x < randomNumber) {
+       console.log("your guess is too low");
+   } else if (x > randomNumber) {
+       console.log("your guess is too high");
+   }
+}
